ea8346e46e65453aaac8bd8c7177391a1b509a05
Baptiste <Baptiste.grias@gmail.com>
2022-05-30 22:57:16 -0700

    fix glitch image

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 window.addEventListener('load', loader);
 
-function loader(){
+function loader() {
+  const TLLOAD = gsap.timeline();
 
-    const TLLOAD = gsap.timeline();
+  TLLOAD.to('.img-container', { height: 400, duration: 1.3, delay: 0.4, ease: 'power2.out' })
+    .to('.bloc-txt', { height: 'auto', duration: 0.6, ease: 'power2.out' }, '-=0.8')
+    .to('.bloc-txt h2', { y: 0, ease: 'power2.out' }, '-=0.6')
 
-    TLLOAD
-    .to('.img-container', {height: 400, duration: 1.3, delay: 0.4, ease: "power2.out"})
-    .to('.bloc-txt', {height: 'auto', duration: 0.6, ease: "power2.out"}, '-=0.8')
-    .to('.bloc-txt h2', {y: 0, ease: "power2.out"}, '-=0.6')
-
-    .to('.flip2', {y: 0, duration: 0.6, ease: "power2.out"})
+    .to('.flip2', { y: 0, duration: 0.6, ease: 'power2.out' })
     .add(() => {
-        document.querySelector('.flip1').style.backgroundImage = "url('../ressources/images/kobe.png')";
+      document.querySelector('.flip1').style.backgroundImage = "url('ressources/images/kobe.png')";
     })
-    .to('.flip2', {y: '-100%'})
+    .to('.flip2', { y: '-100%' })
 
-    .to('.load-container', {opacity: 0, duration: 0.8, delay: 0.7})
+    .to('.load-container', { opacity: 0, duration: 0.8, delay: 0.7 })
     .add(() => {
-        document.querySelector('.load-container').style.display = 'none';
+      document.querySelector('.load-container').style.display = 'none';
     })
     .add(() => {
-        document.querySelector('video').play();
-    }, '-=0.8')
-
+      document.querySelector('video').play();
+    }, '-=0.8');
 }
-   
