7d0e8f470183b2b1281c34337932581927f336c3
Baptiste <Baptiste.grias@gmail.com>
2022-05-30 23:05:16 -0700

    fix a delay anim  in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ window.addEventListener('load', loader);
 function loader() {
   const TLLOAD = gsap.timeline();
 
-  TLLOAD.to('.img-container', { height: 400, duration: 1.3, delay: 0.4, ease: 'power2.out' })
+  TLLOAD.to('.img-container', { height: 400, duration: 1.2, delay: 0.3, ease: 'power2.out' })
     .to('.bloc-txt', { height: 'auto', duration: 0.6, ease: 'power2.out' }, '-=0.8')
     .to('.bloc-txt h2', { y: 0, ease: 'power2.out' }, '-=0.6')
 
@@ -13,7 +13,7 @@ function loader() {
     })
     .to('.flip2', { y: '-100%' })
 
-    .to('.load-container', { opacity: 0, duration: 0.8, delay: 0.7 })
+    .to('.load-container', { opacity: 0, duration: 0.8, delay: 0.5 })
     .add(() => {
       document.querySelector('.load-container').style.display = 'none';
     })
